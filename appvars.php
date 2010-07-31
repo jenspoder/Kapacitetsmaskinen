139cade3ea077ab8ac9fcafb8699ff67afb1e37e
jenspoder <jenspoder@Jens-Poders-MacBook-Pro.local>
2010-07-31 20:27:17 +0200

    Her er en ny version af kodeplanen og første php fil der er bygget til at displaye medarbejdere

---
+++ appvars.php
@@ -0,0 +1,14 @@
+<?php
+
+//hosting og forbindelsesvariabler og andre generelle informationer så man hurtigt kan flytte sitet til et andet sted.
+
+$site_url = "http://localhost:8888/Kapacitetsmaskinen";
+
+$db_host = "localhost";
+$db = "kapacitetsmaskinen";
+$db_user = "root";
+$db_pass = "root";
+
+//generelle variabler
+
+?>
---
+++ medarbejdere.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>  </title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Jens Poder">
+	<!-- Date: 2010-07-31 -->
+</head>
+<body>
+
+<?php 	
+
+/**	
+* @author: Jens Poder (jens.poder@gmail.com)
+* Denne kode håndterer oprettelse og ændring af medarbejdere i Peytz & Co. Kapacitetmaskinen. Det er tanken at lade hele koden køre på denne ene side, der postes til THIS istedet for en masse undersider. Koden består af følgende elementer:
+* 0: Element der inkluderer appvars og classes der skal bruges
+* 1: Element til oprette nye medarbejdere i firmatet (vises hvis $_POST ikke er sat eller hvis $_POST[flag] er 3)
+* 2: Element der lister alle medarbejdere med deres data og link til at rette dem (vises ligegyldigt hvad)
+* 3: Element der skriver bekræftelse på at medarbejder er
+* 4: Element der muliggør rettelse af den enkelte medarbejder
+* Det er tænkt at koden i videst mulige udstrækning skal trække på medarbejder-klassen medarbejder_class.php
+**/
+
+
+
+
+
+?>
+</body>
+</html>
