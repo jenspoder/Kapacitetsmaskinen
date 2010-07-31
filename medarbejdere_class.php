fd204d122d4202ffd57e218341a918896233763f
jenspoder <jens.poder@gmail.com>
2010-07-31 22:33:47 +0200

    er begyndt at rode med at lave en klasse til at holde medarbejder data. Har lidt svært ved at forstå pointen med det her OOP. Vil måske snart se lyset

--- medarbejdere_class.php
+++ medarbejdere_class.php
@@ -1,7 +1,26 @@
 <?php
 
-//her kommer der til at være en klasse
-
-
+	class medarbejder {
+		
+		var $name;
+		var $id;
+		
+		function set_name($new_name) {
+				$this->name = $new_name;
+		}
+					
+		function get_name() {
+			return $this->name;
+		}
+		
+		function save_to_db() {
+			
+		}		
+		
+		function edit_to_db() {
+			
+		}
+		
+	}
 
 ?>
